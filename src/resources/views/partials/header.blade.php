eb568b3ed81bfb91505f1d6027dae9d24b6baaa8
root <root@Ok>
2024-05-06 18:49:10 +0200

    Add questions handling

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -25,6 +25,10 @@
 
                 @auth
                 <!-- Authentication -->
+                <x-button href="{{route('admin')}}" :active="Request::routeIs('admin')">
+                    Panel Admin
+                </x-button>
+
                 <form id="logout-form" action="{{ route('logout') }}" method="POST">
                     @csrf
                     <button type="submit" class="items-center p-2 lg:px-4 md:mx-2 text-green-900 text-center border border-solid border-green-900 rounded hover:bg-green-900 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">
